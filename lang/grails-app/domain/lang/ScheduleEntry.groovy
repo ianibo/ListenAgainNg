ecd1e72e80c143cc91b24e8c1853058b662cca46
Ian Ibbotson <ianibbo@gmail.com>
2017-02-19 18:09:50 +0000

    Filling out test data

--- lang/grails-app/domain/lang/ScheduleEntry.groovy
+++ lang/grails-app/domain/lang/ScheduleEntry.groovy
@@ -2,6 +2,33 @@ package lang
 
 class ScheduleEntry {
 
-    static constraints = {
-    }
+  Staion stn
+  String programmeSlug
+  String programmeName
+  Long start_day
+  Long start_hour
+  Long start_minute
+  Long duration
+
+  static constraints = {
+            owner blank: false, nullable: false // , unique: true
+    programmeSlug blank: false, nullable: true // , unique: true
+    programmeName blank: false, nullable: false // , unique: true
+        start_day blank: false, nullable: false // , unique: true
+       start_hour blank: false, nullable: false // , unique: true
+     start_minute blank: false, nullable: false // , unique: true
+         duration blank: false, nullable: false // , unique: true
+  }
+
+  static mapping = {
+    table 'schedule_entry'
+              stn column: 'se_stn'
+    programmeSlug column: 'se_prog_slug'
+    programmeName column: 'se_prog_name'
+        start_day column: 'se_start_day'
+       start_hour column: 'se_start_hour'
+     start_minute column: 'se_start_minute'
+         duration column: 'se_duration'
+  }
+
 }
--- lang/grails-app/init/lang/BootStrap.groovy
+++ lang/grails-app/init/lang/BootStrap.groovy
@@ -7,7 +7,7 @@ class BootStrap {
   def init = { servletContext ->
     schedulerService.start()
     registerStation('cmr','Celtic Music Radio','http://www.celticmusicradio.net/','http://www.celticmusicradio.net/cmr/celtic_live.m3u');
-    registerProgramme('cmr','Test1');
+    registerProgramme('cmr','Test1',6,18,0,60);
   }
 
   def destroy = {
@@ -17,6 +17,10 @@ class BootStrap {
     def stn = Station.findByName(name) ?: new Station(slug:slug, name:name,stationUrl:stationUrl,streamingUrl:streamUrl).save(flush:true, failOnError:true);
   }
 
-  def registerProgramme(stnSlug, programmeName) {
+  def registerProgramme(stnSlug, programmeName, day, hour, minute, duration) {
+    def stn = Station.findBySlug(stnSlug);
+    def se = ScheduleEntry.findByStnAndProgrammeSlug(stn,programmeSlug)
+    if ( se == null ) {
+    }
   }
 }

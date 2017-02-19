e24065dc5c3cacadf59248cd9a82dd3f90d1c018
Ian Ibbotson <ianibbo@gmail.com>
2017-02-19 17:59:29 +0000

    more test data

--- lang/grails-app/init/lang/BootStrap.groovy
+++ lang/grails-app/init/lang/BootStrap.groovy
@@ -7,6 +7,7 @@ class BootStrap {
   def init = { servletContext ->
     schedulerService.start()
     registerStation('cmr','Celtic Music Radio','http://www.celticmusicradio.net/','http://www.celticmusicradio.net/cmr/celtic_live.m3u');
+    registerProgramme('cmr','Test1');
   }
 
   def destroy = {
@@ -14,5 +15,8 @@ class BootStrap {
 
   def registerStation(slug, name, stationUrl, streamUrl) {
     def stn = Station.findByName(name) ?: new Station(slug:slug, name:name,stationUrl:stationUrl,streamingUrl:streamUrl).save(flush:true, failOnError:true);
+  }
+
+  def registerProgramme(stnSlug, programmeName) {
   }
 }

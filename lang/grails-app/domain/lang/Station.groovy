c4d355790f784d9c5606a41fccc1c103af855e0f
Ian Ibbotson <ianibbo@gmail.com>
2017-02-19 17:17:58 +0000

    updated

--- lang/grails-app/domain/lang/Station.groovy
+++ lang/grails-app/domain/lang/Station.groovy
@@ -2,6 +2,17 @@ package lang
 
 class Station {
 
-    static constraints = {
-    }
+  String name
+  String baseUrl
+
+  static constraints = {
+    baseUrl blank: false, nullable: false // , unique: true
+       name blank: false, nullable: false // , unique: true
+  }
+
+  static mapping = {
+    table 'stn'
+       name column: 'stn_name'
+    baseUrl column: 'stn_base_url'
+  }
 }
